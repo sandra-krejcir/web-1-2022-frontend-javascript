707d3408db980fc5f51b7829a678f6cbf21978d0
cph-mtnl <mtnl@cphbusiness.dk>
2022-08-30 19:41:52 +0200

    exercise descriptions

---
+++ Week 35 (Count project)/script.js
@@ -0,0 +1,15 @@
+/* 1st exercise:
+  Create a h1 title with the text "Your current number".
+  Create a button with an onclick listener.
+    The onclick function should increase a count number, and console log it
+*/
+
+/* 2nd Exercise:
+  Create a keydown listener for enter.
+
+*/
+
+/* 3rd Exercise, solo:
+  Create a "reset" functionality, 
+  by listening for escape, and then reseting the counter to 0.
+*/
